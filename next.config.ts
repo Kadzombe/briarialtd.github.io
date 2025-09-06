25ada1e0683875f3c99ddbd482bf8b1314e17f07
Bria kundai Mboma <briakundaim@gmail.com>
2025-09-06 21:40:44 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,17 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            "fs": false,
-            "net": false,
-            "tls": false,
-        };
-    }
-    return config;
-  }
 };
 
 export default nextConfig;

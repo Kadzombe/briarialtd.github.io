281082c24134c14a62cbb95442891059c8630833
Bria kundai Mboma <briakundaim@gmail.com>
2025-09-06 17:46:30 +0000

    still happening. Can this problem be in my firebase co

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "briaria-ltd-pwng8",
-  "appId": "1:697355277684:web:89ff33fca678b3ac634821",
-  "storageBucket": "briaria-ltd-pwng8.firebasestorage.app",
-  "apiKey": "AIzaSyA23KS8zfrkbgEpIzo0bfe4bSeEctHPjzw",
-  "authDomain": "briaria-ltd-pwng8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "697355277684"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
